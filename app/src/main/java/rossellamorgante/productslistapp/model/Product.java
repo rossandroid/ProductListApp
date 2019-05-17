0c79302ef985f9f0e58eb150b489eb666c19bfef
Rossella Morgante <morgante.rossella@gmail.com>
2019-05-17 21:16:36 +0100

    Creation Model: Product; ProductsList

---
+++ app/src/main/java/rossellamorgante/productslistapp/model/Product.java
@@ -0,0 +1,12 @@
+package rossellamorgante.productslistapp.model;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+    public String name;
+    public String price;
+    public String image;
+    public int id;
+
+}
---
+++ app/src/main/java/rossellamorgante/productslistapp/model/ProductsList.java
@@ -0,0 +1,7 @@
+package rossellamorgante.productslistapp.model;
+import java.io.Serializable;
+import java.util.List;
+
+public class ProductsList implements Serializable {
+    public List<Product> products;
+}
